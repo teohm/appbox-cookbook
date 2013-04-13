c361fb2007ddcc380cacd5b652033fa683e3fd6b
Huiming Teo <teohuiming@gmail.com>
2013-04-13 22:09:31 +0800

    updated readme and changed license to Apache License 2.0

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             "appbox"
 maintainer       "Huiming Teo"
 maintainer_email "teohuiming@gmail.com"
-license          "MIT License"
-description      "Installs/Configures appbox"
+license          "Apache License 2.0"
+description      "Provides a set of recipes to setup a base app server"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 
