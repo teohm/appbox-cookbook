c8b7f74eb20cc99c6a207ed6e97baf3628c0c196
Huiming Teo <teohuiming@gmail.com>
2013-04-13 14:20:23 +0800

    make apps_user a normal user, set apps dir to apps_user home dir, rename devops to admin

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,7 @@
 default["appbox"]["devops_keys"] = []
 default["appbox"]["deploy_keys"] = []
-default["appbox"]["apps_dir"] = "/u/apps"
+default["appbox"]["apps_dir"] = "/home/apps"
 
-default["appbox"]["devops_user"] = "devops"
+default["appbox"]["admin_user"] = "admin"
 default["appbox"]["deploy_user"] = "deploy"
 default["appbox"]["apps_user"] = "apps"
--- recipes/users.rb
+++ recipes/users.rb
@@ -8,33 +8,30 @@
 include_recipe "user"
 
 user_account node["appbox"]["apps_user"] do
-  comment "system user to run apps only"
-  system_user true
-  manage_home false
-  create_group true
+  comment "apps runner"
 end
 user_account node["appbox"]["deploy_user"] do
-  comment "deploys apps only"
+  comment "deployer"
   ssh_keys node["appbox"]["deploy_keys"]
 end
-user_account node["appbox"]["devops_user"] do
-  comment "handles sysadmin operations"
-  ssh_keys node["appbox"]["devops_keys"]
+user_account node["appbox"]["admin_user"] do
+  comment "sysadmin"
+  ssh_keys node["appbox"]["admin_keys"]
 end
 
 group node["appbox"]["apps_user"] do
   action :modify
   members [
     node["appbox"]["apps_user"],
-    node["appbox"]["devops_user"],
+    node["appbox"]["admin_user"],
     node["appbox"]["deploy_user"]
   ]
 end
 
 node.default["authorization"]["sudo"]["groups"] = [
   "sudo",
-  node["appbox"]["deploy_user"],  # TODO workaround enable deploy user to restart server
-  node["appbox"]["devops_user"]
+  node["appbox"]["admin_user"],
+  node["appbox"]["deploy_user"]  # TODO workaround to enable deployer to restart app
 ]
 node.default["authorization"]["sudo"]["passwordless"] = true
 include_recipe "sudo"
